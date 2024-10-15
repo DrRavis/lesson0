2a7be3a3a74ca825c8a879906e7d4d7ffe48287c
Ravis Ratesey <162020276+DrRavis@users.noreply.github.com>
2024-10-15 20:21:59 +0300

    Add files via upload

---
+++ main1.py
@@ -0,0 +1,14 @@
+# 1st program
+print(9**0.5*5)
+# 2nd program
+print(9.99 > 9.98 and 1000 != 1000.1)
+# 3rd program
+a = 2*2+2
+print(a)
+b = 2*(2+2)
+print(b)
+print (a==b)
+# 4th program
+string = '123.456'
+string1 = float(string)
+print(int(string1 *10) % 10)
