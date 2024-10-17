54f0628256ce467b73978e313aaea7eee6a74bea
Ravis Ratesey <162020276+DrRavis@users.noreply.github.com>
2024-10-17 12:35:55 +0300

    Add files via upload

---
+++ lesson217.py
@@ -0,0 +1,5 @@
+completed_hw = 12
+spent_hours = 1.5
+curse_name = 'Python'
+exercise_time = spent_hours/completed_hw
+print(f'Курс: + {curse_name}, всего задач: {completed_hw}, затрачено часов: {spent_hours}, среднее время выполнения: {exercise_time}')
