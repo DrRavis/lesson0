2abedb7958433b102bd121907015036e8fbb1a12
Ravis Ratesey <162020276+DrRavis@users.noreply.github.com>
2024-10-20 23:54:34 +0300

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,6 @@
+immutable_var = (1, 2, 'apple', True)
+print(immutable_var)
+mutable_list = [11, 12, 21, 'grapes', 'peach', 'banana']
+mutable_list[3] = 33
+mutable_list[-1] = 'apple'
+print(mutable_list)
